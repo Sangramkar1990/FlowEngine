750fa1bc4f169aa11ab16d272621eb7d35264d67
Sangram Kar <machineshop.tech@gmail.com>
2025-05-08 10:57:56 +0530

    cors bugfix

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(cookieParser());
 
 // Enable CORS with credentials and explicit origin
 app.use(cors({
-  origin: 'http://localhost:3000', // Replace '*' with your frontend URL
+  origin: 'http://localhost:5173', // Replace '*' with your frontend URL
   credentials: true
 }));
 
